17891c8dd5e0ba917590a1cf1d4d1d5183729a12
Harshi <harshitha.a988@gmail.com>
2026-05-11 20:01:55 +0530

    Day 7 Implemented email notifications and scheduling

---
+++ backend/src/main/java/com/internship/tool/service/MailService.java
@@ -0,0 +1,31 @@
+package com.internship.tool.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(String to, String subject, String body) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+
+    @Scheduled(cron = "0 0 9 * * ?")
+    public void sendDailyReminder() {
+
+        System.out.println("Daily reminder triggered");
+    }
+}
